a750ab57c1b999ac244645673ed0089cbfb1cc78
Joshua <joshuamacleod@live.com>
2020-11-15 22:50:03 +0000

    Step 10; updated style, but temperature constantly resets

--- interface.js
+++ interface.js
@@ -1,4 +1,35 @@
 $(document).ready(function() {
   var thermostat = new Thermostat();
-  $('#temperature').text(thermostat.temperature);
+  updateTemperature();
+
+  $('#temperature-up').click(function() {
+    thermostat.up();
+    updateTemperature();
+  });
+
+  $('#temperature-down').click(function() {
+    thermostat.down();
+    updateTemperature();
+  });
+
+  $('#temperature-reset').click(function() {
+    thermostat.resetTemperature();
+    updateTemperature();
+  });
+
+  $('#powersaving-on').click(function() {
+    thermostat.switchPowerSavingModeOn();
+    $('#power-saving').text('on')
+    updateTemperature();
+  })
+
+  $('#powersaving-off').click(function() {
+    thermostat.switchPowerSavingModeOff();
+    $('#power-saving').text('off')
+    updateTemperature();
+  })
+
+  function updateTemperature() {
+    $('#temperature').text(thermostat.temperature);
+  };
 });
